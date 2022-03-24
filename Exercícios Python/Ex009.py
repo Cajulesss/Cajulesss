69474d627d1eadfe4e36c299ea078d78ddb7a953
Cajulesss <99684656+Cajulesss@users.noreply.github.com>
2022-03-24 19:52:54 -0300

    Exercício 05
    Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada
    OBS: coloquei a primitiva float, invés do int, caso a pessoa queira também saber o valor da multiplicação em números com vírgula.

---
+++ Exercícios Python/Ex009.py
@@ -0,0 +1,26 @@
+#Faça um programa que leia um número inteiro  qualquer e mostre na tela a sua tabuada
+
+#Perguntar ao usuário o número a ser calculado:
+Numt = float(input("Digite um número:"))
+t1 = Numt*1
+t2 = Numt*2
+t3 = Numt*3
+t4 = Numt*4
+t5 = Numt*5
+t6 = Numt*6
+t7 = Numt*7
+t8 = Numt*8
+t9 = Numt*9
+t10 = Numt*10
+
+#printar:
+print("O número digitado multiplicado por 1 equivale a:{}".format(t1))
+print("O número digitado multiplicado por 2 equivale a:{}".format(t2))
+print("O número digitado multiplicado por 3 equivale a:{}".format(t3))
+print("O número digitado multiplicado por 4 equivale a:{}".format(t4))
+print("O número digitado multiplicado por 5 equivale a:{}".format(t5))
+print("O número digitado multiplicado por 6 equivale a:{}".format(t6))
+print("O número digitado multiplicado por 7 equivale a:{}".format(t7))
+print("O número digitado multiplicado por 8 equivale a:{}".format(t8))
+print("O número digitado multiplicado por 9 equivale a:{}".format(t9))
+print("O número digitado multiplicado por 10 equivale a:{}".format(t10))
