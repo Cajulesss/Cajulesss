53fa661d4bc2373b11582767a9124bd713c91fff
Cajulesss <99684656+Cajulesss@users.noreply.github.com>
2022-03-25 20:37:13 -0300

    Exercício 07
    Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário,com 15% de aumento.

---
+++ Exercícios Python/Ex013.py
@@ -0,0 +1,6 @@
+#Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário,com 15% de aumento.
+
+Perg = int(input("Digite seu salário:"))
+por = 0.15*Perg
+aumento = por+Perg
+print("Seu novo salário com 15 % ""de aumento é:{}".format(aumento))
