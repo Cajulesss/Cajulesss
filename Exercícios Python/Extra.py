e4b84082c1331c1f1231af537d23de05969f5ae6
Cajulesss <99684656+Cajulesss@users.noreply.github.com>
2022-03-25 19:12:50 -0300

    Extra :)
    2- Em uma academia de ginástica, a uma promoção para novos alunos: 10% de desconto na primeira mensalidade se a mesma for paga juntamente com a matrícula.Se a primeira mensalidade for paga até 10 dias depois da matrícula deverá ser pago o valor integral de R$150,00.Já no caso de o pagamento da primeira mensalidade ser feito de 11 à 30 dias após à matrícula há um acréscimo de 10% nesse valor.
    Gabriela se matriculou nessa academia e efetuou o pagamento da primeira mensalidade 15 dias após a matrícula.Qual é o valor da primeira mensalidade que Gabriela pagou?

---
+++ Exercícios Python/Extra.py
@@ -0,0 +1,8 @@
+#2- Em uma academia de ginástica, a uma promoção para novos alunos: 10% de desconto na primeira mensalidade se a mesma for paga juntamente com a matrícula.Se a primeira mensalidade for paga até 10 dias depois da matrícula deverá ser pago o valor integral de R$150,00.Já no caso de o pagamento da primeira mensalidade ser feito de 11 à 30 dias após à matrícula há um acréscimo de 10% nesse valor.
+#Gabriela se matriculou nessa academia e efetuou o pagamento da primeira mensalidade 15 dias após a matrícula.Qual é o valor da primeira mensalidade que Gabriela pagou?
+
+#10% de 150,00:
+integral = 150
+por = 0.10*integral
+subtr = integral+por
+float = print(("O valor da primeira mensalidade que Gabriela pagou foi de: {}".format(subtr)))
