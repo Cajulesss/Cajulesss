0d754c2ce972b09606a4a91d9e9844018ac88753
Cajulesss <99684656+Cajulesss@users.noreply.github.com>
2022-03-30 16:04:47 -0300

    Ex014
    Faça a conversão de graus Celcius para Fahrenheit

---
+++ Exercícios Python/Ex014.py
@@ -0,0 +1,5 @@
+"""Faça a conversão de graus Celcius para Fahrenheit.
+OBS: FÓRMULA: C = 5*(F-32)/9"""
+
+Celcius = int(input("Digite a temperatura em °C para ser convertida em °F:"))
+print("A temperatura em °F é : {} °C".format(5*(Celcius-32)/9))
